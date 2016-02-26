30eb72a21ed100183909a88ed2a7d342abfb32b1
roels005 <roels005@umn.edu>
2016-02-25 20:18:13 -0600

    Update Board.java

--- Project2/src/Board.java
+++ Project2/src/Board.java
@@ -2,4 +2,26 @@
  * Created by jakesnyder-hansen on 2/24/16.
  */
 public class Board {
+  private int boarddimensions;
+    private Object[][] boardarray;
+    public Board(int x){
+        this.boarddimensions = x;
+    }
+    public Board(){
+        this.boarddimensions = 3;
+    }
+    public void intializeboard(){
+        Object[] boardarray = Object[boarddimensions][boarddimensions];
+    }
+    public int getboarddimensions(){
+        return boarddimensions;
+    }
+    public void setboarddimensions(int x){
+        boarddimensions = x;
+    }
+    public void setpieces(Pieces x){
+        boardarray[x.getXlocation()][x.getYlocation()] = x;
+    }
+
+
 }
