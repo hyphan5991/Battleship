1aaa60df808799dafda2bf27cacf24560d6ddd3e
hyphan5991 <j.snyderhansen.321@gmail.com>
2016-03-02 13:25:19 -0600

    Update BoardArray.java

--- Project2/src/BoardArray.java
+++ Project2/src/BoardArray.java
@@ -8,16 +8,18 @@
 public class BoardArray {
 
 
-    private int[][] boardArray;
-    public static int[][] shipArray;
+    private static int[][] boardArray;
     public static int EMPTY = 0;
     public static int SHIP = 1;
     public static int MINE = 2;
     public static int SHIP_HIT = 3;
     public static int EMPTY_HIT = 4;
     public static int MINE_HIT = 5;
+    public static int shipNumber;
+    public static int num;
 
     public BoardArray(int x, int y) {
+        num = 0;
         if (x > 10) {
             x = 10;
         } else if (y > 10) {
@@ -36,7 +38,7 @@ public class BoardArray {
         }
 
         int max = x * y;
-        int num;
+
         if (max <= 16) {
             num = 1;
 
@@ -45,151 +47,151 @@ public class BoardArray {
         } else {
             num = 3;
         }
-        int numShips = num * 3;
+        shipNumber = num * 3;
         int sumShips = 0;
 
-            int i = 0;
-            boolean passed3 = false;
-            while (i < num) {
+        int i = 0;
+        boolean passed3 = false;
+        while (i < num) {
 
-                int shipX = findX(x);
-                int shipY = findY(y);
-                if (boardArray[shipY][shipX] == EMPTY) {
+            int shipX = findX(x);
+            int shipY = findY(y);
+            if (boardArray[shipY][shipX] == EMPTY) {
 
-                    boolean passed2 = false;
-                    int j = 0;
-                    int orientpassed = 6;
+                boolean passed2 = false;
+                int j = 0;
+                int orientpassed = 6;
 
-                    while (!passed2 && j < 50) {
+                while (!passed2 && j < 50) {
 
-                        int orient = (int) Math.round((5 * Math.random()));
+                    int orient = (int) Math.round((5 * Math.random()));
 
-                        if (orient == 0) {
-                            try {
-                                if (boardArray[shipY - 1][shipX] == EMPTY && boardArray[shipY - 2][shipX] == EMPTY) {
-                                    passed2 = true;
-                                } else {
-                                    passed2 = false;
-                                }
-                            } catch (ArrayIndexOutOfBoundsException b) {
-                                passed2 = false;
-                            } catch (IndexOutOfBoundsException e) {
+                    if (orient == 0) {
+                        try {
+                            if (boardArray[shipY - 1][shipX] == EMPTY && boardArray[shipY - 2][shipX] == EMPTY) {
+                                passed2 = true;
+                            } else {
                                 passed2 = false;
                             }
-
-                        } else if (orient == 1) {
-                            try {
-                                if (boardArray[shipY][shipX + 1] == EMPTY && boardArray[shipY][shipX + 2] == EMPTY) {
-                                    passed2 = true;
-                                } else {
-                                    passed2 = false;
-                                }
-                            } catch (ArrayIndexOutOfBoundsException b) {
-                                passed2 = false;
-                            } catch (IndexOutOfBoundsException e) {
-                                passed2 = false;
-                            }
-                        } else if (orient == 2) {
-                            try {
-                                if (boardArray[shipY + 1][shipX] == EMPTY && boardArray[shipY + 2][shipX] == EMPTY) {
-                                    passed2 = true;
-                                } else {
-                                    passed2 = false;
-                                }
-                            } catch (ArrayIndexOutOfBoundsException b) {
-                                passed2 = false;
-                            } catch (IndexOutOfBoundsException e) {
-                                passed2 = false;
-                            }
-
-                        } else if (orient == 3) {
-                            try {
-                                if (boardArray[shipY][shipX - 1] == EMPTY && boardArray[shipY][shipX - 2] == EMPTY) {
-                                    passed2 = true;
-                                } else {
-                                    passed2 = false;
-                                }
-                            } catch (ArrayIndexOutOfBoundsException b) {
-                                passed2 = false;
-                            } catch (IndexOutOfBoundsException e) {
-                                passed2 = false;
-                            }
-
-                        } else if (orient == 4) {
-                            try {
-                                if (boardArray[shipY - 1][shipX] == EMPTY && boardArray[shipY + 1][shipX] == EMPTY) {
-                                    passed2 = true;
-                                } else {
-                                    passed2 = false;
-                                }
-                            } catch (ArrayIndexOutOfBoundsException b) {
-                                passed2 = false;
-                            } catch (IndexOutOfBoundsException e) {
-                                passed2 = false;
-                            }
-
-                        } else if (orient == 5) {
-                            try {
-                                if (boardArray[shipY][shipX + 1] == EMPTY && boardArray[shipY][shipX - 1] == EMPTY) {
-                                    passed2 = true;
-                                } else {
-                                    passed2 = false;
-                                }
-                            } catch (ArrayIndexOutOfBoundsException b) {
-                                passed2 = false;
-                            } catch (IndexOutOfBoundsException e) {
-                                passed2 = false;
-                            }
-
+                        } catch (ArrayIndexOutOfBoundsException b) {
+                            passed2 = false;
+                        } catch (IndexOutOfBoundsException e) {
+                            passed2 = false;
                         }
-                        orientpassed = orient;
-                        j++;
-                    }
 
-                    if (passed2) {
-                        switch (orientpassed) {
-                            case 0:
-                                boardArray[shipY][shipX] = SHIP;
-                                boardArray[(shipY - 1)][shipX] = SHIP;
-                                boardArray[(shipY - 2)][shipX] = SHIP;
-                                break;
-                            case 1:
-                                boardArray[shipY][shipX] = SHIP;
-                                boardArray[shipY][(shipX + 1)] = SHIP;
-                                boardArray[shipY][(shipX + 2)] = SHIP;
-                                break;
-                            case 2:
-                                boardArray[shipY][shipX] = SHIP;
-                                boardArray[shipY + 1][shipX] = SHIP;
-                                boardArray[shipY + 2][shipX] = SHIP;
-                                break;
-                            case 3:
-                                boardArray[shipY][shipX] = SHIP;
-                                boardArray[shipY][(shipX - 1)] = SHIP;
-                                boardArray[shipY][(shipX - 2)] = SHIP;
-                                break;
-                            case 4:
-                                boardArray[shipY][shipX] = SHIP;
-                                boardArray[(shipY - 1)][shipX] = SHIP;
-                                boardArray[(shipY + 1)][shipX] = SHIP;
-                                break;
-                            case 5:
-                                boardArray[shipY][shipX] = SHIP;
-                                boardArray[shipY][(shipX + 1)] = SHIP;
-                                boardArray[shipY][(shipX - 1)] = SHIP;
-                                break;
-                            default:
-                                System.out.print("something went wrong");
-                                break;
-
+                    } else if (orient == 1) {
+                        try {
+                            if (boardArray[shipY][shipX + 1] == EMPTY && boardArray[shipY][shipX + 2] == EMPTY) {
+                                passed2 = true;
+                            } else {
+                                passed2 = false;
+                            }
+                        } catch (ArrayIndexOutOfBoundsException b) {
+                            passed2 = false;
+                        } catch (IndexOutOfBoundsException e) {
+                            passed2 = false;
                         }
-                        i++;
+                    } else if (orient == 2) {
+                        try {
+                            if (boardArray[shipY + 1][shipX] == EMPTY && boardArray[shipY + 2][shipX] == EMPTY) {
+                                passed2 = true;
+                            } else {
+                                passed2 = false;
+                            }
+                        } catch (ArrayIndexOutOfBoundsException b) {
+                            passed2 = false;
+                        } catch (IndexOutOfBoundsException e) {
+                            passed2 = false;
+                        }
+
+                    } else if (orient == 3) {
+                        try {
+                            if (boardArray[shipY][shipX - 1] == EMPTY && boardArray[shipY][shipX - 2] == EMPTY) {
+                                passed2 = true;
+                            } else {
+                                passed2 = false;
+                            }
+                        } catch (ArrayIndexOutOfBoundsException b) {
+                            passed2 = false;
+                        } catch (IndexOutOfBoundsException e) {
+                            passed2 = false;
+                        }
+
+                    } else if (orient == 4) {
+                        try {
+                            if (boardArray[shipY - 1][shipX] == EMPTY && boardArray[shipY + 1][shipX] == EMPTY) {
+                                passed2 = true;
+                            } else {
+                                passed2 = false;
+                            }
+                        } catch (ArrayIndexOutOfBoundsException b) {
+                            passed2 = false;
+                        } catch (IndexOutOfBoundsException e) {
+                            passed2 = false;
+                        }
+
+                    } else if (orient == 5) {
+                        try {
+                            if (boardArray[shipY][shipX + 1] == EMPTY && boardArray[shipY][shipX - 1] == EMPTY) {
+                                passed2 = true;
+                            } else {
+                                passed2 = false;
+                            }
+                        } catch (ArrayIndexOutOfBoundsException b) {
+                            passed2 = false;
+                        } catch (IndexOutOfBoundsException e) {
+                            passed2 = false;
+                        }
+
                     }
+                    orientpassed = orient;
+                    j++;
                 }
+
+                if (passed2) {
+                    switch (orientpassed) {
+                        case 0:
+                            boardArray[shipY][shipX] = SHIP;
+                            boardArray[(shipY - 1)][shipX] = SHIP;
+                            boardArray[(shipY - 2)][shipX] = SHIP;
+                            break;
+                        case 1:
+                            boardArray[shipY][shipX] = SHIP;
+                            boardArray[shipY][(shipX + 1)] = SHIP;
+                            boardArray[shipY][(shipX + 2)] = SHIP;
+                            break;
+                        case 2:
+                            boardArray[shipY][shipX] = SHIP;
+                            boardArray[shipY + 1][shipX] = SHIP;
+                            boardArray[shipY + 2][shipX] = SHIP;
+                            break;
+                        case 3:
+                            boardArray[shipY][shipX] = SHIP;
+                            boardArray[shipY][(shipX - 1)] = SHIP;
+                            boardArray[shipY][(shipX - 2)] = SHIP;
+                            break;
+                        case 4:
+                            boardArray[shipY][shipX] = SHIP;
+                            boardArray[(shipY - 1)][shipX] = SHIP;
+                            boardArray[(shipY + 1)][shipX] = SHIP;
+                            break;
+                        case 5:
+                            boardArray[shipY][shipX] = SHIP;
+                            boardArray[shipY][(shipX + 1)] = SHIP;
+                            boardArray[shipY][(shipX - 1)] = SHIP;
+                            break;
+                        default:
+                            System.out.print("something went wrong");
+                            break;
+
+                    }
+                    i++;
+                }
+            }
 //                else {
 //                    i = 0;
 //                }
-            }
+        }
         int j = 0;
 
         while (j < num){
@@ -198,8 +200,8 @@ public class BoardArray {
             if (boardArray[mineY][mineX] == EMPTY) {
                 boardArray[mineY][mineX] = MINE;
                 j++;
-                }
             }
+        }
 
     }
 
@@ -226,8 +228,16 @@ public class BoardArray {
         this.boardArray = BOARDARRAY;
     }
 
+    public static int getShipNumber() {
+        return shipNumber;
+    }
 
-
+    public static void setShipNumber(int shipNumber) {
+        BoardArray.shipNumber = shipNumber;
+    }
+    public static int[][] getBoardArray() {
+        return boardArray;
+    }
     public static void main(String[] args) {
         BoardArray tp = new BoardArray(4, 5);
         for (int i = 0; i < tp.getBOARDARRAY()[0].length; i++) {
@@ -254,5 +264,3 @@ public class BoardArray {
 
     }
 }
-
-
